42b366116a4350f0a08574f980e41707bf1c217a
ksy777ksy7-tech <ksy777ksy7@gmail.com>
2026-02-13 02:10:46 +0300

    initial commit1

--- src/main.java
+++ src/main.java
@@ -1,11 +1,11 @@
 public class main {
     public static void main(String[] args) {
 
-        int amount = 13676;
+        int amount = 13676; // сумма покупки
 
-        int rub = 20;
+        int rub = 20; // каждые 20 руб дают 1 бонус
 
-        int bonus = amount / rub;
+        int bonus = amount / rub; // сумма бонусов
 
         System.out.println(bonus);
 
