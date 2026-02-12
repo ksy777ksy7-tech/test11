4c459c1744cd5b810043bc71b05befd500df33f8
ksenny <cnhfecbrf@mail.ru>
2026-02-13 00:53:01 +0300

    initial commit

---
+++ src/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+
+        int amount = 13676;
+
+        int rub = 20;
+
+        int bonus = amount / rub;
+
+        System.out.println(bonus);
+
+    }
+}
